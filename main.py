eea0ce1692469fd68b713d3ffe96cf72c22c743c
master <pospeev.artem@icloud.com>
2023-03-15 03:01:13 +0400

    mvp with async

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import asyncio
 import re
 import time
 from typing import Optional
 
+import aiohttp
 import requests
 from bs4 import BeautifulSoup, Tag
 
@@ -26,26 +28,17 @@ def get_sentence_for_link(a_tag: Tag) -> str:
     return a_text
 
 
-def get_links_text(url: str) -> dict:
+def get_links_text(text: str) -> dict:
     '''
     Ищет все ссылки из блоков <p> внутри контейнера с ID = SEARCH_ID (см. settings.py)
     Возвращает в словаре вида:
     {'a1.href': 'a1_text', 'a2.href': 'a2_text', 'a3.href': 'a3_text', ... }
-    :param url: str (url)
+    :param text: str (url)
     :return: dict
     '''
     res = dict()
 
-    try:
-        response = requests.get(url, headers=HEADERS)
-    except Exception as e:
-        logger.error(f'Во время выполнения запроса к {URL_DOMAIN + url} возникла ошибка {e}')
-        exit()
-
-    logger.info(f'Посещен url адрес {url}')
-    time.sleep(0.1)
-
-    p_tags = BeautifulSoup(response.text, 'html.parser').find(id=SEARCH_ID).find_all('p')
+    p_tags = BeautifulSoup(text, 'html.parser').find(id=SEARCH_ID).find_all('p')
 
     for tag in p_tags:
         for a_tag in tag.find_all('a', href=True):
@@ -55,43 +48,63 @@ def get_links_text(url: str) -> dict:
     return res
 
 
-def find_path(start_url: str, end_url: str) -> Optional[list]:
+async def get_page(session: aiohttp.ClientSession, url: str, path: Optional[list]) -> tuple:
     '''
-    Принимает точку старта и искомый url, ищет оптимальный путь поиском в ширину.
-    Возвращает структуру [(text info, URL address), (text info, URL address) ..]
-    :param start_url: URL стартовой страницы
-    :param end_url: Искомый URL
-    :return: list(tuple(text, url), tuple(text, url)...)
+    Отправляет запрос по указанному адресу, возвращает html код страницы
+    :param session: aiohttp.ClientSession
+    :param url: url address
+    :param path: использую для общей логики, чтобы данные по пути запроса были привязаны к странице
+    :return: (response.text, path)
     '''
-    queue = [(start_url, [])]
+    async with session.get(url) as r:
+        logger.info(f'Посещена страница {url}')
+        r_text = await r.text()
+        return r_text, path
+
+
+# TODO вопросы по этой работе этой функции
+async def find_path(session, start_url, end_url) -> Optional[list]:
+    '''
+    Принимает стартовый адрес и искомый, ищет путь между ними
+    :param session: aiohttp.ClientSession
+    :param start_url: start url
+    :param end_url: finish url
+    :return: возвращает либо путь, либо ничего
+    '''
+    tasks = list()
+    urls = [(start_url, [])]
     visited = set()
-    visited.add(start_url)
+    while urls:
 
-    while queue:
-        url, path = queue.pop(0)
-
-        if url.startswith('/wiki/'):
-            url = URL_DOMAIN + url
-        if url == end_url:
-            return path
-        if len(path) >= 3:
-            continue
-
-        links_text = get_links_text(url)
-
-        for next_url, next_text in links_text.items():
-            if not next_url.startswith('/wiki'):
+        while urls:  # <-- Цикл для заполнения задач
+            url, path = urls.pop(0)
+            if url.startswith('/wiki/'):
+                url = URL_DOMAIN + url
+            if url == end_url:
+                return path
+            if len(path) >= 3:
                 continue
-            visited.add(next_url)
-            next_path = path + [(next_text, URL_DOMAIN + next_url)]
+            if url not in visited:
+                task = asyncio.create_task(get_page(session, url, path))
+                tasks.append(task)
 
-            queue.append((next_url, next_path))
-
+        pages = await asyncio.gather(*tasks)
+        if pages:
+            for page in pages:
+                page, path = page
+                links_text = get_links_text(page)
+                for next_url, next_text in links_text.items():
+                    if not next_url.startswith('/wiki'):
+                        continue
+                    if next_url not in visited:
+                        visited.add(next_url)
+                        next_path = path + [(next_text, URL_DOMAIN + next_url)]
+                        urls.append((next_url, next_path))
     return None
 
 
-def main():
-    start_url, end_url = input('Введите стартовый URL: '), input('Введите URL, путь до которого требуется найти: ')
+async def main():
+    start_url, end_url = 'https://ru.wikipedia.org/wiki/Xbox_360_S', 'https://ru.wikipedia.org/wiki/Nintendo_3DS'
 
     if not start_url.startswith(URL_DOMAIN) \
             or not end_url.startswith(URL_DOMAIN):
@@ -99,7 +112,8 @@ def main():
         exit()
 
     logger.info("Ищу путь...")
-    path = find_path(start_url, end_url)
+    async with aiohttp.ClientSession() as session:
+        path = await find_path(session, start_url, end_url)
 
     if path is None:
         logger.info("Путь не найден :(")
@@ -111,4 +125,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
